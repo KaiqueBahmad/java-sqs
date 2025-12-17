9a6981cebd86d238b6853b7185c29ad64b7a53a2
Kaique Bahmad <kaiquebahmadt@gmail.com>
2025-12-17 03:25:01 -0300

    feat: adiciona modelo Pedido e configuração AWS SQS
    - Adiciona dependência spring-cloud-aws-starter-sqs
    - Cria entidade JPA Pedido com UUID
    - Substitui application.properties por exemplo com configurações completas
    - Inclui configurações para H2, JPA, AWS SQS e logging

---
+++ src/main/java/kaiquebt/dev/javasqs/model/Pedido.java
@@ -0,0 +1,26 @@
+package kaiquebt.dev.javasqs.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "pedido")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    
+    
+}
